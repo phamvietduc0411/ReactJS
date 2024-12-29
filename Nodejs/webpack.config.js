78be4bc93ac851d0e2862d6cd8a1fcbb4232a334
Viet-Duc-Pham <phamvietduc0411@gmail.com>
2024-12-29 14:59:08 +0700

    Webpack & Webpack CLI

---
+++ Nodejs/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  // Xác định chế độ chạy của Webpack, có thể là 'development' hoặc 'production'.
+  // Điều này giúp Webpack tối ưu hóa quá trình đóng gói tùy thuộc vào môi trường.
+  mode: 'development',
+  // Xác định điểm đầu vào (entry point) của ứng dụng.
+  // Đây là tệp JavaScript hoặc tệp chính của ứng dụng mà Webpack sẽ bắt đầu đóng gói.
+  entry: {
+    app: path.resolve('./src/index.js')
+  },
+  // Nơi Webpack sẽ lưu các tệp đã được đóng gói và tên của các tệp đó.
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'script.js'
+  }
+};

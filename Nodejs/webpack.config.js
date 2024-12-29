660fee0e44d6625955f3a00e9b1fdf07487e1f5f
Viet-Duc-Pham <phamvietduc0411@gmail.com>
2024-12-29 15:43:11 +0700

    handle Loader& CSS in webpack

--- Nodejs/webpack.config.js
+++ Nodejs/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   // Xác định chế độ chạy của Webpack, có thể là 'development' hoặc 'production'.
   // Điều này giúp Webpack tối ưu hóa quá trình đóng gói tùy thuộc vào môi trường.
-  mode: 'development',
+  mode: 'production',
   // Xác định điểm đầu vào (entry point) của ứng dụng.
   // Đây là tệp JavaScript hoặc tệp chính của ứng dụng mà Webpack sẽ bắt đầu đóng gói.
   entry: {
@@ -12,6 +12,14 @@ module.exports = {
   // Nơi Webpack sẽ lưu các tệp đã được đóng gói và tên của các tệp đó.
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'script.js'
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss|css$/,
+        use: ['style-loader', 'css-loader','sass-loader']
+      }
+    ]
   }
 };

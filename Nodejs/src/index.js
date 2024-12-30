099e5c7db2f21f2e6d241a5dd858df6a550eff60
Viet-Duc-Pham <phamvietduc0411@gmail.com>
2024-12-30 21:32:11 +0700

    Babel

--- Nodejs/src/index.js
+++ Nodejs/src/index.js
@@ -2,3 +2,16 @@ import './styles/style.css'
 import './styles/style.scss'
 
 console.log('check source map ');
+
+let sum = (num1) => { 
+    console.log(num1+22);
+    
+}
+
+sum(23)
+
+const person = { name: 'Duoc' }
+const personClone = { ...person }
+console.log('personClone', personClone)
+
+console.log('Object.values', Object.values(personClone))

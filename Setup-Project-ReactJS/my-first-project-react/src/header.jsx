5a25f41f4c9807f55d1c29e48fa590ccfe41d8c8
Viet-Duc-Pham <phamvietduc0411@gmail.com>
2025-01-01 20:30:48 +0700

    JSX

---
+++ Setup-Project-ReactJS/my-first-project-react/src/header.jsx
@@ -0,0 +1,11 @@
+const lastName = 'Vit'
+
+const element = <h1>Hello world!!! I am {lastName} </h1>
+
+const element2 = (
+  <a href='gogle.com' className='element2'>
+    Google
+  </a>
+)
+
+const element3 = <h3 />

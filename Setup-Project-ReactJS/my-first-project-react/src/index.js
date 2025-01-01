67d52bd8cb98f13fdb5eb21ba59366c015469e52
Viet-Duc-Pham <phamvietduc0411@gmail.com>
2025-01-01 20:52:37 +0700

    render element

--- Setup-Project-ReactJS/my-first-project-react/src/index.js
+++ Setup-Project-ReactJS/my-first-project-react/src/index.js
@@ -4,12 +4,27 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+//const element = <h1>Create New Element</h1>
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+
+var tick = () => {
+  const element = (
+    <div>
+      <h1>Hello world!!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  )
+  root.render(element)
+}
+
+setInterval(tick, 1000)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
